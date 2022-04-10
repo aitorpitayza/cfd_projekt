398476e5787d4adf1234c12b0badf816f960f0c7
lmprapal <101739516+lmprapal@users.noreply.github.com>
2022-04-10 20:19:49 +0200

    Retoques
    Pequeños cambios en variables y creación de la función integración temporal que hace llamada a la funcción convección.

--- scripts/convec.m
+++ scripts/convec.m
@@ -1,42 +1,37 @@
-% function [ C , C_BC_arriba] = convec( V, Area,...
-%     Normales, N, Vecinos,  BCtop, v, cv, rho)
-% 
-% % Esta función entrega la matriz de convección del dominio, así como la 
-% % parte convectiva relativa a las condiciones de contorno.
-% 
-% %   Entradas
-% %   ........
-% %
-% %   - N: número de celdas.
-% %   - V: vector que contiene las áreas de las celdas de la malla 
-% %   - Area: matriz que contiene la longitud de cada lado de las celdas.
-% %   - Normales: matriz que contiene los vectores normales de cada lado de
-% %               las celdas y la celda vecina a cada vector normal.
-% %   - Rc: matriz que contiene las coordenadas del centroide de cada celda.
-% %   - Rn: matriz que contiene las coordenadas de los nodos de la malla.
-% %   - Vecinos: matriz que contiene las celdas vecinas de cada elemento de 
-% %              la malla.
-% %   - t: instante temporal.
-% %   - kk: conductividad térmica [W/mK].
-% %   - rho: densidad del fluido [kg/m^3].
-% %   - cv: calor específico a volumen constante [J/kgK].
-% %   - v: matriz de dimensión N filas y dos columnas que contiene las 
-% %        componentes de la velocidad en m/s en los centroides de cada celda 
-% %        de la malla.
-% %
-% %
-% %   Salida
-% %   ......
-% %
-% %   - C: matriz de convección del problema.
-% %   - C_BC_arriba: matriz que contiene la parte convectiva asociada a las
-% %           condiciones de contorno en el límite superior.
-% 
+function [ C , C_BC_arriba] = convec( Vol, Areas,...
+    Normales, N, Vecinos,  BCtop, v, cv, rho)
+
+% Esta función entrega la matriz de convección del dominio, así como la 
+% parte convectiva relativa a las condiciones de contorno.
+
+%   Entradas
+%   ........
+%
+%   - N: número de celdas.
+%   - Vol: vector que contiene las áreas de las celdas de la malla 
+%   - Area: matriz que contiene la longitud de cada lado de las celdas.
+%   - Normales: matriz que contiene los vectores normales de cada lado de
+%               las celdas y la celda vecina a cada vector normal. 
+%   - Rn: matriz que contiene las coordenadas de los nodos de la malla.
+%   - Vecinos: matriz que contiene las celdas vecinas de cada elemento de 
+%              la malla.
+%   - t: instante temporal.
+%   - rho: densidad del fluido [kg/m^3].
+%   - cv: calor específico a volumen constante [J/kgK].
+%   - v: matriz de dimensión N filas y dos columnas que contiene las 
+%        componentes de la velocidad en m/s en los centroides de cada celda 
+%        de la malla.
+%
+%
+%   Salida
+%   ......
+%
+%   - C: matriz de convección del problema.
+%   - C_BC_arriba: matriz que contiene la parte convectiva asociada a las
+%           condiciones de contorno en el límite superior.
+
  Vecinos=;
  Normales=; 
- Rc = grid_preproc.centroid; %%%Llamar a la input Rc, pero lanzar desde fuera con grid_preproc,etc, para que sirva la documentacion
- Areas = grid_preproc.areas; %%%ídem
- V =;%%%ídem
  %%Preguntar como está montado la matriz conectividad para ajustar a
  %%vecinos
  C = zeros(N, N);
@@ -54,8 +49,8 @@
              conv =  -A*vn*rho*cv ;
 
  
-                     C(i,k) = conv / V(i) ; %Flujo de energía entrando en la celda i
-                     C(k,k) = C(k,k) - conv / V(k); %Flujo saliendo de la celda k
+                     C(i,k) = conv / Vol(i) ; %Flujo de energía entrando en la celda i
+                     C(k,k) = C(k,k) - conv / Vol(k); %Flujo saliendo de la celda k
                      
             end
          end
---
+++ scripts/integracion.m
@@ -0,0 +1,3 @@
+%[ C , C_BC_arriba] = convec( grid_preproc.volumes, grid_preproc.areas,Normales, N, Vecinos(adaptar),  BCtop?, v, cv, rho)
+%He añadido la llamada a convec que se hará desde este script de
+%integración temporal, cuando esté adaptado se descomentará.
