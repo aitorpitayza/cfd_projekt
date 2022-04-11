e4125b252d049129ab892f941dee51d6a99be220
lmprapal <101739516+lmprapal@users.noreply.github.com>
2022-04-11 18:47:15 +0200

    Función convección terminada.
    Terminada la función que calcula las matrices de convección para estimar la derivada temporal mediante volúmenes finitos para el dominio y las CC salientes.

--- scripts/convec.m
+++ scripts/convec.m
@@ -1,5 +1,6 @@
 function [ C , C_BC_arriba] = convec( Vol, Areas,...
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
     Normales, N, Vecinos,  BCtop, v, cv, rho)
 =======
     Normalesx,Normalesy, N, Vecinos,  BCtop, v, cv, rho)
@@ -7,6 +8,15 @@ function [ C , C_BC_arriba] = convec( Vol, Areas,...
 
 % Esta función entrega la matriz de convección del dominio, así como la 
 % parte convectiva relativa a las condiciones de contorno.
+=======
+   
+    Normalesx,Normalesy, N, Vecinos,  BCtop, v, cv, rho)
+
+
+% Esta función entrega la matriz de convección del dominio, así como la 
+% parte convectiva relativa a las condiciones de contorno, utilizada
+%para estimar la derivada temporal de T mediante volúmenes finitos.
+>>>>>>> Stashed changes
 
 %   Entradas
 %   ........
@@ -16,16 +26,28 @@ function [ C , C_BC_arriba] = convec( Vol, Areas,...
 %   - Area: matriz que contiene la longitud de cada lado de las celdas.
 %   - Normales: matriz que contiene los vectores normales de cada lado de
 %               las celdas y la celda vecina a cada vector normal. 
+<<<<<<< Updated upstream
 %   - Rn: matriz que contiene las coordenadas de los nodos de la malla.
 %   - Vecinos: matriz que contiene las celdas vecinas de cada elemento de 
 %              la malla.
 %   - t: instante temporal.
+=======
+%   
+%   - Vecinos: matriz que contiene las celdas vecinas de cada elemento de 
+%              la malla.
+%   
+>>>>>>> Stashed changes
 %   - rho: densidad del fluido [kg/m^3].
 %   - cv: calor específico a volumen constante [J/kgK].
 %   - v: matriz de dimensión N filas y dos columnas que contiene las 
 %        componentes de la velocidad en m/s en los centroides de cada celda 
 %        de la malla.
+<<<<<<< Updated upstream
 %
+=======
+ %  -BCtop:vector que contiene las celdas situadas en el límite
+%            superior del dominio.
+>>>>>>> Stashed changes
 %
 %   Salida
 %   ......
@@ -34,6 +56,7 @@ function [ C , C_BC_arriba] = convec( Vol, Areas,...
 %   - C_BC_arriba: matriz que contiene la parte convectiva asociada a las
 %           condiciones de contorno en el límite superior.
 
+<<<<<<< Updated upstream
  Vecinos=;
 <<<<<<< Updated upstream
  Normales=; 
@@ -42,6 +65,11 @@ function [ C , C_BC_arriba] = convec( Vol, Areas,...
 >>>>>>> Stashed changes
  %%Preguntar como está montado la matriz conectividad para ajustar a
  %%vecinos
+=======
+
+
+ 
+>>>>>>> Stashed changes
  C = zeros(N, N);
  for i = 1:N
      for j = 1:3
@@ -50,9 +78,17 @@ function [ C , C_BC_arriba] = convec( Vol, Areas,...
              k = Vecinos(i,j); %índice de la celda vecina a la celda i en la cara j
      
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
              n = Normales(i,j); %Normal a la cara j de la celda i
 =======
              n = [Normalesx(i,j),Normalesy(i,j)]; %Normal a la cara j de la celda i
+>>>>>>> Stashed changes
+=======
+
+             n = Normales(i,j); %Normal a la cara j de la celda i
+
+             n = [Normalesx(i,j),Normalesy(i,j)]; %Normal a la cara j de la celda i
+
 >>>>>>> Stashed changes
              vn = dot(v(i,:),n);
              
@@ -68,6 +104,7 @@ function [ C , C_BC_arriba] = convec( Vol, Areas,...
          end
      end
  end
+<<<<<<< Updated upstream
 % C = sparse(C);
 % 
 % % Concicion de contorno de flujo saliente (arriba)
@@ -89,3 +126,26 @@ function [ C , C_BC_arriba] = convec( Vol, Areas,...
 % C_BC_arriba = sparse(C_BC_arriba);
 % 
 % end
+=======
+C = sparse(C);
+
+% Concicion de contorno de flujo saliente (arriba)
+C_BC_arriba = zeros( N, N );
+
+for i = 1:length( BCtop )
+    k = BCtop( i );
+    
+    for j = 1:3
+        if Normales(k,3*j-2)==0
+            
+            C_BC_arriba( k,k ) = - Areas( k, 2*j-1 ) * v(k,2)/ Vol(k);
+            
+        end
+    end
+end
+
+C_BC_arriba = sparse(C_BC_arriba);
+
+
+end
+>>>>>>> Stashed changes
--- scripts/integracion.m
+++ scripts/integracion.m
@@ -1,7 +1,18 @@
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
 %[ C , C_BC_arriba] = convec( grid_preproc.volumes, grid_preproc.areas,Normales, N, Vecinos(adaptar),  BCtop?, v, cv, rho)
 =======
 %[ C , C_BC_arriba] = convec( grid_preproc.volumes, grid_preproc.areas,grid_preproc.nx,grid_preproc.ny, N, Vecinos(adaptar),  BCtop?, v, cv, rho)
 >>>>>>> Stashed changes
 %He añadido la llamada a convec que se hará desde este script de
 %integración temporal, cuando esté adaptado se descomentará.
+=======
+
+%   - BC_arriba: vector que contiene las celdas situadas en el límite
+%            superior del dominio.
+
+[ C , C_BC_arriba] = convec( grid_preproc.volumes, grid_preproc.areas,grid_preproc.nx,grid_preproc.ny, N, grid_preproc.connectivity,  BC_arriba, v, cv, rho)
+
+%He añadido la llamada a convec que se hará desde este script de
+%integración temporal.
+>>>>>>> Stashed changes
