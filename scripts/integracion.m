a6bb45d82706b1ac93f1161099b239c6a866c332
lmprapal <101739516+lmprapal@users.noreply.github.com>
2022-04-22 12:00:46 +0200

    eliminación mierda

--- scripts/integracion.m
+++ scripts/integracion.m
@@ -1,12 +1,4 @@
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-%[ C , C_BC_arriba] = convec( grid_preproc.volumes, grid_preproc.areas,Normales, N, Vecinos(adaptar),  BCtop?, v, cv, rho)
-=======
-%[ C , C_BC_arriba] = convec( grid_preproc.volumes, grid_preproc.areas,grid_preproc.nx,grid_preproc.ny, N, Vecinos(adaptar),  BCtop?, v, cv, rho)
->>>>>>> Stashed changes
-%He añadido la llamada a convec que se hará desde este script de
-%integración temporal, cuando esté adaptado se descomentará.
-=======
+
 
 %   - BC_arriba: vector que contiene las celdas situadas en el límite
 %            superior del dominio.
@@ -15,4 +7,4 @@
 
 %He añadido la llamada a convec que se hará desde este script de
 %integración temporal.
->>>>>>> Stashed changes
+
