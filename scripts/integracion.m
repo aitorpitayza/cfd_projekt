a230ede803da67a7d6c48f1e36de8220ca0dc074
Aitor Pitarch Ayza <aitor.pitayza@gmail.com>
2022-04-22 14:30:10 +0200

    Reordenación

--- scripts/integracion.m
+++ scripts/integracion.m
@@ -3,7 +3,9 @@
 %   - BC_arriba: vector que contiene las celdas situadas en el límite
 %            superior del dominio.
 
-[ C , C_BC_arriba] = convec( grid_preproc.volumes, grid_preproc.areas,grid_preproc.nx,grid_preproc.ny, N, grid_preproc.connectivity,  BC_arriba, v, cv, rho)
+[ C , C_BC_arriba] = convec( grid_preproc.volumes,...
+ grid_preproc.areas,grid_preproc.nx,grid_preproc.ny,...
+  N, grid_preproc.connectivity,  BC_arriba, v, cv, rho)
 
 %He añadido la llamada a convec que se hará desde este script de
 %integración temporal.
