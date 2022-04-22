dc6e079a2bd340bbf2d451aa5acd980838d9d348
Aitor Pitarch Ayza <aitor.pitayza@gmail.com>
2022-04-22 17:49:45 +0200

    BCtop added

--- scripts/main.m
+++ scripts/main.m
@@ -10,7 +10,6 @@ filenames = ["../data/nodes_4.dat" ...
 grid_data = grid_loader(filenames);
 
 grid = mesher(grid_data);
-
-bc_top(grid.nodes, grid.cells)
+grid.BCtop = bc_top(grid.nodes, grid.cells);
 
 toc
